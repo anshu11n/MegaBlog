12c9ca63aad78e8a8720ac7d3c2def22d7e2a1d0
anshu11n <nauwalaanshu11@gmail.com>
2024-01-27 11:58:45 +0530

    Logo set

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div>
+      Logo
+      {/* <img src='../images/blog.png' width="100px" height="100px" alt='Mega Blog logo'/> */}
+    </div>
   )
 }
 
